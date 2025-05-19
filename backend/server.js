82b0c43966320a008ee7d93c5e13ba84f6583b82
ritambrayadav <ritambrayadav20@gmail.com>
2025-05-19 13:17:14 +0530

    changes related to lamda

--- backend/server.js
+++ backend/server.js
@@ -2,26 +2,37 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
-import ServerlessHttp from "serverless-http";
+import serverless from "serverless-http";
+
 import authRoutes from "./routes/authRoutes.js";
 import noteRoutes from "./routes/notesRoutes.js";
 import errorHandler from "./middlewares/errorMiddleware.js";
+
 dotenv.config();
+
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use((err, req, res, next) => {
-  res.status(res.statusCode || 500).json({ message: err.message });
-});
+
 app.get("/", (req, res) => {
   res.send("Backend is Running!");
 });
+
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", noteRoutes);
 
 app.use(errorHandler);
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+app.use((err, req, res, next) => {
+  res.status(res.statusCode || 500).json({ message: err.message });
 });
+
+export const handler = serverless(app);
+
+if (process.env.NODE_ENV !== "lambda") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
